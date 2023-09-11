46e316d23d034b11ffff603a09d71d724fb539ad
Karolina <karolina.anna.jesionek@gmail.com>
2023-09-11 13:28:46 +0200

    second commit

---
+++ js/gallery.js
@@ -0,0 +1,8 @@
+// external js: masonry.pkgd.js
+
+$('.grid').masonry({
+    itemSelector: '.grid-item',
+    columnWidth: 160,
+    gutter: 20
+  });
+  
---
+++ js/main.js
@@ -0,0 +1,15 @@
+const footerYear = document.querySelector('.footer__year');
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+handleCurrentYear();
+
+
+
+
+
+
+
