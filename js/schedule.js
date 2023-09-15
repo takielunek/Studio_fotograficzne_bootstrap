a7f2f939c8b40003c11e97c3d7e5382294e7f9c4
Karolina <karolina.anna.jesionek@gmail.com>
2023-09-15 11:17:55 +0200

    eighth commit

--- js/schedule.js
+++ js/schedule.js
@@ -1,32 +1,24 @@
 const date = new Date();
-
 const renderCalendar = () => {
   date.setDate(1);
-
   const monthDays = document.querySelector(".days");
-
   const lastDay = new Date(
     date.getFullYear(),
     date.getMonth() + 1,
     0
   ).getDate();
-
   const prevLastDay = new Date(
     date.getFullYear(),
     date.getMonth(),
     0
   ).getDate();
-
   const firstDayIndex = date.getDay();
-
   const lastDayIndex = new Date(
     date.getFullYear(),
     date.getMonth() + 1,
     0
   ).getDay();
-
   const nextDays = 7 - lastDayIndex - 1;
-
 const months = [
 'Styczeń',
 'Luty',
@@ -41,17 +33,12 @@ const months = [
 'Listopad',
 'Grudzień',
 ];
-
 document.querySelector(".date h1").innerHTML = months[date.getMonth()];
-
 document.querySelector(".date p").innerHTML = new Date().toDateString();
-
 let days = "";
-
 for (let x = firstDayIndex; x > 0; x--) {
   days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
 }
-
 for (let i = 1; i <= lastDay; i++) {
   if (
     i === new Date().getDate() &&
@@ -65,18 +52,14 @@ for (let i = 1; i <= lastDay; i++) {
   monthDays.innerHTML = days;
 for (let j = 1; j <= nextDays; j++) {
   days += `<div class="next-date">${j}</div>`;
-
 }
 };
-
 document.querySelector(".prev").addEventListener("click", () => {
 date.setMonth(date.getMonth() - 1);
 renderCalendar();
 });
-
 document.querySelector(".next").addEventListener("click", () => {
 date.setMonth(date.getMonth() + 1);
 renderCalendar();
 });
-
 renderCalendar();
